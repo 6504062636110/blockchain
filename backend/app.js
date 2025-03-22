5ea5ce81185178f716719544f17fe59153877b1a
tonkaew131 <nongtonkaew@gmail.com>
2025-03-22 20:00:19 +0700

    Prettier

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const { ethers } = require("ethers");
-const contractABI = require("./artifacts/contracts/hello-world.sol/HelloWorld.json"); 
+const contractABI = require("./artifacts/contracts/hello-world.sol/HelloWorld.json");
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
@@ -16,32 +16,31 @@ const contract = new ethers.Contract(contractAddress, contractABI.abi, wallet);
 app.use(cors());
 app.use(express.json());
 
-
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
 
-app.get("/status", async(req, res) => {
+app.get("/status", async (req, res) => {
     const message = await contract.message();
     res.status(200).send({
         status: true,
-        message: 'I LOVE CAT',
+        message: "I LOVE CAT",
         data: {
-            message
-        }
-    })
+            message,
+        },
+    });
 });
 app.post("/update", async (req, res) => {
     try {
-      const { newMessage } = req.body;
-      const tx = await contract.update(newMessage);
-      await tx.wait();
-      res.json({ txHash: tx.hash, newMessage });
+        const { newMessage } = req.body;
+        const tx = await contract.update(newMessage);
+        await tx.wait();
+        res.json({ txHash: tx.hash, newMessage });
     } catch (error) {
-      res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
-  });
-  
+});
+
 // app.post("/messages", (req, res) => {
 
 //     console.log( req.body );
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,23 +1,23 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require('dotenv').config();
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 /**
-* @type import('hardhat/config').HardhatUserConfig
-*/
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-   solidity: "0.7.3",
-   defaultNetwork: "ganache",
-   networks: {
-      hardhat: {
-         chainId: 1337
-      },
-      ganache: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
-   },
-   paths: {
-      artifacts: "./artifacts"
-   }
-}
+    solidity: "0.7.3",
+    defaultNetwork: "ganache",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
+        ganache: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
+    paths: {
+        artifacts: "./artifacts",
+    },
+};
